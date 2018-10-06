fd08c53409befaade02d56ca69d271aab638d09f
Michael Borthwick <mhborthwick@gmail.com>
2018-10-05 23:56:42 -0700

    get range of numbers and pinpong to display on submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
+// business logic
+function list(a){
+  if (a%15===0){
+		return "pingpong";
+		}
+	else if(a%3===0){
+		return "ping";
+		}
+	else if(a%5===0){
+		return "pong";
+		}
+	else{
+		return a;
+		}
+};
+
+// user logic
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var numberInput = $("input#numInput").val();
-
-    $("#answers").text(numberInput);
-
-
-    $(".answersContainer").show();
-
     event.preventDefault();
+    var numberInput = parseInt($("input#numInput").val());
+    for(var i=1; i<=numberInput; i++){
+      $("#answers").append(list(i));
+    };
   });
 });
