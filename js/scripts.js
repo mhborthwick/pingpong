7ca83630e5cebb924182842077353783dfbfc38b
Michael Borthwick <mhborthwick@gmail.com>
2018-10-06 10:48:03 -0700

    create an alert if NAN is entered on submit

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@
 function list(x){
   if (x%15===0){
 		return "<li>"+"pingpong"+"</li>";
-		}
+  }
 	else if(x%3===0){
 		return "<li>"+"ping"+"</li>";
-		}
+  }
 	else if(x%5===0){
 		return "<li>"+"pong"+"</li>";
-		}
+  }
 	else{
 		return "<li>"+x+"</li>";
-		}
+  }
 };
 
 // user logic
@@ -19,8 +19,12 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     event.preventDefault();
     var numberInput = parseInt($("input#numInput").val());
+    if (isNaN(numberInput)){
+      alert("Try entering in a number instead! 😉");
+    } else {
     for(var i=1; i<=numberInput; i++){
       $("#answers").append(list(i));
+      }
     };
   });
 });
